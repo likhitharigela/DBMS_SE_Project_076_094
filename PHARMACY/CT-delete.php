6434c82d0feb4fc74cf50263ad716a5647f8d318
Likhith Arigela <131606335+likhitharigela@users.noreply.github.com>
2024-11-14 18:58:12 +0530

    Delete PHARMACY directory

--- PHARMACY/CT-delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include "config.php";
-	$sql="DELETE FROM clinicaltrials WHERE trial_id='$_GET[id]'";
-	if ($conn->query($sql))
-	header("location:CT-view.php");
-	else
-	echo "error";
-?>
--- PHARMACY/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-		$conn = mysqli_connect("localhost", "root", "LikH@24#", "pharmacy");
-		if ($conn->connect_error) {
-			die("Connection failed: " . mysqli_connect_error());
-
-		}
-?>
